aeb4b2c2c40b90538a2f41268b35f0240ca7f920
Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-11-15 01:52:50 +0300

    I loved your first project. and wanted to make a contribution that adds percentage processing support and output function.

---
+++ Modules/process.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def add(x,y):
+	return x+y
+
+def subtract(x,y):
+	return x-y
+
+def multiply(x,y):
+	return x*y
+
+def division(x,y):
+	return x/y
+
+def percentage(x,y):
+	return x%y
